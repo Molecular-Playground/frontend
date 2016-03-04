dabc03e4f6272d9bb63aada8912962340505c5c8
Carter Tiernan <ctiernan@umass.edu>
2016-03-03 19:58:05 -0500

    changed route to templates not gary and I react project

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log(path.join(__dirname+'/../views/app.html'));
-  res.sendFile(path.join(__dirname+'/../views/app.html'));
+	console.log(path.join(__dirname+'/../views/index.html'));
+  res.sendFile(path.join(__dirname+'/../views/index.html'));
 });
 
 module.exports = router;
