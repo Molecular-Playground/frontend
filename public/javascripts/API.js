416d361adf2f7034b58172a56b9a5cb56c1f2530
Gary P White <GaryPWhite@live.com>
2016-03-04 15:47:02 -0500

    LoginSection is reverted back to 'un-modified' version, the sections should contain what is obvious by the name of the react file, where 'LoginSection' contains a login form. Containing extra information is a schema violation'

--- public/javascripts/API.js
+++ public/javascripts/API.js
@@ -4,11 +4,22 @@
 
 var api = {
   login : function(params){
-    // just returning dummy data until it works and whatnot.
+    if (!(params.data)) // throw error on empty data input
+      throw new error("must have data to send with /api/login");
+    /*// just returning dummy data until it works and whatnot.
     params.success({
         token : "totally legit JWT"
+    });*/
+    // send request to server with proper paramaters.
+    $.ajax({
+      contentType: "application/json",
+      dataType : "json",
+      method : "POST",
+      url : "192.168.99.100:8000/api/login",
+      success : params.success || function (data) {console.log("data uncaught for /api/login");},
+      error : params.error || function (data) {/* error will be handled by browser*/},
+      data : params.data
     });
-    // hint hint $.ajax({});
   }
 };
 
