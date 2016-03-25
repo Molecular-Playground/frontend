17d9dba44e31c104cd2b7daaa33551879517f11b
Matt <mifische@umass.edu>
2016-03-25 16:37:38 -0400

    made login into a dialog

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,14 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/../views/index.html'));
 });
 
+router.get('/signup', function(req, res, next) {
+	console.log(path.join(__dirname+'/../views/register.html'));
+  res.sendFile(path.join(__dirname+'/../views/register.html'));
+});
+
+router.get('/login', function(req, res, next) {
+	console.log(path.join(__dirname+'/../views/login.html'));
+  res.sendFile(path.join(__dirname+'/../views/login.html'));
+});
+
 module.exports = router;
