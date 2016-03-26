b36050aece7056fd35c3faba0295e75e05e4bd29
Gary P White <GaryPWhite@live.com>
2016-03-25 20:16:53 -0400

    Fixed the server, should work as expected now.

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,16 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/../views/index.html'));
 });
 
+/* GET login page. */
+router.get('/login', function(req,res,next) {
+	console.log(path.join(__dirname+'/../views/login.html'));
+  res.sendFile(path.join(__dirname+'/../views/login.html'));
+});
+
+/* GET register page. */
+router.get('/register', function(req,res,next){
+	console.log(path.join(__dirname+'/../views/register.html'));
+	res.sendFile(path.join(__dirname+'/../views/register.html'));
+});
+
 module.exports = router;
