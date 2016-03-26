42836577b43a1bd45fe8edd893e5890bf9cc9517
Gary P White <GaryPWhite@live.com>
2016-03-25 21:59:12 -0400

    set up watchify/browserify for angular instead. angular dev can start, looking for the login page...

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var MolApp = require('./components/MolApp.react');
-var LoginSection = require('./components/LoginSection.react');
-var LandingComponent = require('./components/LandingComponent.react');
-var Router = require('react-router').Router;
-var Route = require('react-router').Route;
-var IndexRoute = require('react-router').IndexRoute;
-var browserHistory = require('react-router').browserHistory;
-var IndexRoute = require('react-router').IndexRoute;
+(function() {
+  var angular = require('angular');
 
-ReactDOM.render((
-  <Router history ={browserHistory}>
-    <Route path="/" component={MolApp}>
-      <IndexRoute component={LandingComponent}/>
-      <Route path="/login" component={LoginSection}/>
-    </Route>
-  </Router>
-),document.getElementById('MolApp'));
+  angular.module('MolApp', []);
+
+})();
