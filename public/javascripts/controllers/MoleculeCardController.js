f7e945226ead125730d933606e43343d24741e39
Andrew Marchetti <amarchet@umass.edu>
2016-04-07 19:04:48 -0400

    molecules almsot there

--- public/javascripts/controllers/MoleculeCardController.js
+++ public/javascripts/controllers/MoleculeCardController.js
@@ -2,4 +2,9 @@ var angular = require('angular');
 
 angular.module('MolApp').controller('moleculeCardCtrl', ['$scope', '$element', function($scope, $element){
     $scope.width = .8*($element.context.parentNode.offsetWidth);
+	$scope.descriptionShown = true;
 }]);
+
+
+
+
