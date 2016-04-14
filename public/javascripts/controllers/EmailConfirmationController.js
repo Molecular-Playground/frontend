c45e03b4fb8ba8a087f60bfb65ab9e977dc2a72d
Carter Tiernan <ctiernan@umass.edu>
2016-04-14 18:57:25 -0400

    working email verification

--- public/javascripts/controllers/EmailConfirmationController.js
+++ public/javascripts/controllers/EmailConfirmationController.js
@@ -1,21 +1,26 @@
 var angular = require('angular');
 
-angular.module('MolApp').controller('EmailConfirmationCtrl', ['$scope', 'userService', '$routeParams', function($scope, userService, $routeParams){
-  //console.log($routeParams.test)
-  validate();
-  $scope.validate = function(e){
+angular.module('MolApp').controller('EmailConfirmationCtrl', ['$scope', '$routeParams', 'userService',  function($scope, $routeParams, userService){
+  $scope.success = false;
+
+
+  function validate(e){
+    //cosole.log($roteParams.email);
     if ($routeParams.email && $routeParams.key){
       userService.validate({
         data : {
-          email : $routeParams.email.trim(),
-          password : $routeParams.key.trim()
+          key : $routeParams.key,
+          email : $routeParams.email
         },
         success : function(resp){
-          //console.log(resp);
+          console.log(resp);
+          $scope.sucess = true;
         },
         // TODO : display errors when backend gets back about ms-users ticket for login error reporting.
       });
     }
-  };
+  }
+
+  validate();
 
 }]);
