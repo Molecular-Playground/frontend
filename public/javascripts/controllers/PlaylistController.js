ed5b401fb1d525bfed73fe61e76c14ff568dfc87
Matt <mifische@umass.edu>
2016-04-14 19:14:15 -0400

    added playlist creation functionality (needs to be stored in backend somehow?)

--- public/javascripts/controllers/PlaylistController.js
+++ public/javascripts/controllers/PlaylistController.js
@@ -2,38 +2,51 @@ var angular = require('angular');
 
 angular.module('MolApp').controller('PlaylistsCtrl', ['$scope', 'userService', function($scope, userService){
 
-  var molList1 = ["DNA", "RNA", "Test"];
+  $scope.temp = "";
 
-  this.playLists = [
-  {
-    'name' : 'Proteins',
-    'molList' : [
-        {
-          'name' : 'Zero Calorie Fat II'
-        },
-        {
-          'name' : 'RNA Polymerase'
-        },
-        {
-          'name' : 'Ribosome'
-        },
-        {
-          'name' : 'Acetophenone'
-        }
-      ]   
-    },
-    {
-    'name' : 'Acids',
-    'molList' : [
-        {
-          'name' : 'DNA'
-        },
-        {
-          'name' : 'HIV Protease'
-        }
-      ]   
-    }
-  ];
+  $scope.items = [];
+  // {
+  //   'name' : 'Proteins',
+  //   'molList' : [
+  //   {
+  //     'name' : 'Zero Calorie Fat II'
+  //   },
+  //   {
+  //     'name' : 'RNA Polymerase'
+  //   },
+  //   {
+  //     'name' : 'Ribosome'
+  //   },
+  //   {
+  //     'name' : 'Acetophenone'
+  //   },
+  //   {
+  //     'name' : 'whatever'
+  //   }
+  //   ]   
+  // },
+  // {
+  //   'name' : 'Acids',
+  //   'molList' : [
+  //   {
+  //     'name' : 'DNA'
+  //   },
+  //   {
+  //     'name' : 'HIV Protease'
+  //   }
+  //   ]   
+  // }
+  // ];
+
+  $scope.addItem = function () {
+
+    $scope.items.push({
+      'name' : $scope.temp
+    });
+    $scope.temp = "";
+
+  };
+
 
   componentHandler.upgradeDom();
 }]);
