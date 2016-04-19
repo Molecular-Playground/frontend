50eb0de7fa23d5038dd5f1c08f01d09f1fef2e12
Mary Moser <mmoser@umass.edu>
2016-04-19 17:22:26 -0400

    manual checkboxes

--- public/javascripts/controllers/ScheduleController.js
+++ public/javascripts/controllers/ScheduleController.js
@@ -3,16 +3,19 @@ var angular = require('angular');
 angular.module('MolApp').controller('ScheduleCtrl', ['$scope', '$location', 'userService', function($scope, $location, userService){
 	$scope.playlists = [
 		{
+			select:false,
 			name : "PlayList ONE",
 			startTime : "8:00 AM",
 			endTime : "10:00 AM"
 		},
 		{
+			select:false,
 			name : "PlayList TWO",
 			startTime : "10:00 AM",
 			endTime : "12:00 PM"
 		},
 		{
+			select:false,
 			name : "PlayList THREE",
 			startTime : "12:00 PM",
 			endTime : "4:00 PM"
