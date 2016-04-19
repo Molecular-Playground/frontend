5f934044896441c8657ceddb0b0d460fd4364fef
Gary P White <GaryPWhite@live.com>
2016-04-19 19:25:17 -0400

    added functionality to ensure that the side menu closes when a destination is clicked

--- public/javascripts/controllers/NavigationMenuController.js
+++ public/javascripts/controllers/NavigationMenuController.js
@@ -3,6 +3,9 @@ var angular = require('angular');
 angular.module('MolApp').controller('NavigationMenuCtrl', ['$rootScope', '$scope', 'userService', function($rootScope, $scope, userService){
   $scope.userIsLoggedIn = userService.isLoggedIn();
 
+  $scope.closeMenu = function(){
+    $(".mdl-layout__drawer-button").click();
+  };
 
   var updateScope = function(){
     $scope.userIsLoggedIn = userService.isLoggedIn();
